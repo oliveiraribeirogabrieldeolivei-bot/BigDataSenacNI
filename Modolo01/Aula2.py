f953474a4e0332be075cd7c92b128ac08194ab00
GABRIEL DE OLIVEIRA RIBEIRO <gabriel.oribeiro@aluno.senac.rio>
2025-10-27 20:10:55 -0300

    commitado

---
+++ Modolo01/Aula2.py
@@ -0,0 +1,60 @@
+#moto = True
+#if moto == True:
+ #   print('Rebeca está chegando, pai')
+
+#elif preco:
+#   print('')
+
+#else:
+ #   print('Hoje não deu, prof!':)
+
+
+#if preco 
+
+
+#Questão Número 2
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segundo nota: '))
+nota3 = float(input('Digite a segunda nota:'))
+nota4 = float(input('Digite a quarta nota: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+if media > 7:
+    print(f'Aprovado: {media}')
+
+elif media >= 5 or media >= 7:
+    print(f'recuperação:{media}')
+
+else:
+    print(f'reprovado {media}')
+
+#contratando
+#Leve saúde
+
+#questão Número 1
+
+#nu1 = float(input('Digite a primeira nota: '))
+#nu2 = float(input('Digite a segundo nota: '))
+#nu3 = float(input('Digite a terceiro nota:'))
+
+#if nu1 > nu2 and nu1 > nu3:
+#    if nu2 > nu3:
+#        print(f'1- {nu3}\n2- {nu2}\n3- {nu1}')
+#    else:
+#        print(f'1- {nu2}\n2- {nu3}\n3- {nu1}')
+
+#elif nu2 > nu1 and nu2 > nu3:
+#    if nu1 > nu3:
+#        print(f'1- {nu3}\n2- {nu1}\n3- {nu2}')
+#    else:
+#        print(f'1- {nu1}\n2- {nu3}\n3- {nu2}')
+
+#else:
+#    if nu1 > nu2:
+#        print(f'1- {nu2}\n2- {nu1}\n3- {nu3}')
+#    else:
+#        print(f'1- {nu1}\n2- {nu2}\n3- {nu3}')
+
+
+
---
+++ Modolo01/aula1.py
@@ -0,0 +1,43 @@
+#aula 1= variaveis em python (22/10/25)
+
+## categorias:
+
+#idade = 30#int
+#nome='Maria'#string
+#preco='19.99'#float
+#estoque= True #boolean
+#estoque_distponivel = None #nulo
+
+#print(idade)
+#print(type(idade))
+
+#cadastro = int(input('Digite seu Nome: '))
+#print(cadastro)
+#print(type(cadastro))
+
+#cd MODOLO01 entra pasta
+#cd.. sair da pasta
+#py para entra no arquivo
+
+#nota1 = float(input('Digite a primeira nota: '))
+#nota2 = float(input('Digite a segundo nota: '))
+#media = (nota1 + nota2) / 2
+
+#print(media)
+
+nt1 = float(input('Digite a primeira nota: '))
+nt2 = float(input('Digite a segundo nota: '))
+
+soma = nt1 + nt2
+sub = nt1 - nt2
+mult = nt1 * nt2
+div = nt1 / nt2
+modulo= nt1 % nt2
+
+print('A soma é:', soma)
+print('A sub é:', sub)
+print('A mult é:', mult)
+print('A div é:', div)
+print(f'A módulo é:{modulo}')
+
+#Enap escola 
